5a9ae938d7bd2a6eed4043be9e6026b4cacba3c9
maliut <lqn619@163.com>
2019-09-21 16:55:29 +0800

    add bottom bar & small fix

---
+++ commonelem.js
@@ -0,0 +1,31 @@
+const common = {
+    title: `
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">明日方舟作业帮</a>
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
+    `,
+    bottom: `
+<nav class="m-bottom-bar">
+    <div class="container">
+        &copy;&nbsp;2019&nbsp;
+        <a href="https://github.com/maliut" target="_blank">@maliut</a>&nbsp;
+        <a href="https://github.com/FinZhang" target="_blank">@fin</a>&nbsp;|&nbsp;
+        <a href="https://github.com/maliut/arknights" target="_blank"><i class="icon-github"></i><a>&nbsp;
+        <a href="https://weibo.com/maliut" target="_blank"><i class="icon-sina-weibo"></i><a>&nbsp;
+        <a tabindex="0" role="button" data-toggle="popover" data-placement="top" data-trigger="focus" data-html="true"
+        data-content="<img src='mp.jpg' style='width:150px;height:150px'></img>"><i class="icon-wechat"></i></a>
+    </div>
+</nav>
+    `
+};
